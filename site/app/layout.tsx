fe6ad4af74ab5fd061398247ab8c4ba491bb21a6
Sunghyun Cho <hey@cho.sh>
2026-02-12 08:40:23 +0900

    feat: Add collapsible animations and dropdown menu components

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -1,9 +1,19 @@
 import "./globals.css";
+import Script from 'next/script';
+import {Analytics} from '@vercel/analytics/react';
+import {SpeedInsights} from '@vercel/speed-insights/react';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return children;
+  return (
+    <>
+      {children}
+      <Analytics />
+      <SpeedInsights />
+      <Script src="https://cdn.visitors.now/v.js" data-token="850b7c05-9254-4a71-9631-32865ae1daf2" />
+    </>
+  );
 }
